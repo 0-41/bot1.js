1370ccfeb3e1df99db3536e7d6ce80f2b466fbd9
aziz127182 <32429099+aziz127182@users.noreply.github.com>
2018-01-27 15:55:27 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+   client.user.setStatus(" online");
+
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
